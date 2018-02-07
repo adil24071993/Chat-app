2db38ba6edab124b49a672f67410bd2fb8ea3cf9
Syed Adil <a.syedadil@gmail.com>
2018-02-07 11:58:26 +0530

    Add users list

--- public/js/chat.js
+++ public/js/chat.js
@@ -33,6 +33,14 @@ socket.on('disconnect', function() {
   console.log('Disconencted from server');
 });
 
+socket.on('updateUserList', function(users){
+  var ol = $('<ol></ol>');
+  users.forEach(function(user){
+    ol.append($('<li></li>').text(user));
+  });
+
+  $('#users').html(ol);
+});
 
 socket.on('newMessage', function(message){
   var formattedTime = moment(message.createdAt).format('h:mm a');
